efb71c17f61432fdeac4b7a770e7ec46f740560b
MosraRakesh <rakeshtech234@gmail.com>
2020-12-16 10:41:43 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http"
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductsAlertComponent } from "./products-alert/products-alert.component";
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { DoublePipe } from './double.pipe';
-import { DividebyPipe } from './divideby.pipe';
-import { CartService } from './cart.service';
-import { CartComponent } from './cart/cart.component';
-import { ShippingComponent } from './shipping/shipping.component';
-import { GeneralErrorComponent } from './general-error/general-error.component';
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { DoublePipe } from "./double.pipe";
+import { DividebyPipe } from "./divideby.pipe";
+import { CartService } from "./cart.service";
+import { CartComponent } from "./cart/cart.component";
+import { ShippingComponent } from "./shipping/shipping.component";
+import { GeneralErrorComponent } from "./general-error/general-error.component";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
   imports: [
@@ -22,12 +23,13 @@ import { GeneralErrorComponent } from './general-error/general-error.component';
     HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: "", component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-      {path:'cart',component:CartComponent},
-      { path: 'shipping', component: ShippingComponent },
-      { path: 'generalerror', component: GeneralErrorComponent }
-      ])
+      { path: "products", component: ProductListComponent },
+      { path: "products/:productId", component: ProductDetailsComponent },
+      { path: "cart", component: CartComponent },
+      { path: "shipping", component: ShippingComponent },
+      { path: "generalerror", component: GeneralErrorComponent },
+      { path: "", component: LoginComponent }
+    ])
   ],
   declarations: [
     AppComponent,
@@ -40,7 +42,7 @@ import { GeneralErrorComponent } from './general-error/general-error.component';
     CartComponent,
     ShippingComponent,
     GeneralErrorComponent,
-   
+    LoginComponent
   ],
   bootstrap: [AppComponent],
   providers: [CartService]
